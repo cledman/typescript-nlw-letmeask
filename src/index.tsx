febf05964638beafff056d1793f55dd2e7b9f779
cledson <cledman@gmail.com>
2021-06-23 00:23:27 -0300

    Contexts createad

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './services/firebase';
+import './styles/global.scss';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +10,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
